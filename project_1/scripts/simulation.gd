5f7ace3860348e8c0082565958087c120009aad8
Peter Spiegler <peter.spiegler@consiliari.de>
2024-08-28 17:15:49 +0200

    Random Spawn n-particles in box

---
+++ project_1/scripts/simulation.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var collision_damping : float = 1;
+@export var bounds_size : Vector2 = Vector2(600, 400);
+@export var particle_size : float = 20; 
+@export var gravity : float = 980; 
+@export var number_of_particles: int = 20;
+@export var starting_density: int = 5;
+
+var bounding_rectangle: Rect2 = Rect2(Vector2(position.x - bounds_size.x / 2, position.y - bounds_size.y / 2), Vector2(bounds_size.x, bounds_size.y));
+
+var scene_to_instance = preload("res://scenes/water_drop.tscn")
+
+func _ready() -> void:
+	var squareSide = ceil(pow(number_of_particles, 1.0/2.0));
+	var singleParticleSpace = (particle_size + starting_density);
+	var squareOrigin = floor((squareSide / 2)) * -singleParticleSpace;
+	var drawn = 0;
+	for i in squareSide:
+		for j in squareSide:
+			if(drawn >= number_of_particles): continue;
+			var x = squareOrigin + j * singleParticleSpace;
+			var y = squareOrigin + i * singleParticleSpace;
+			add_child(Water_Drop.new_water_drop(Vector2(x, y), collision_damping, bounding_rectangle, particle_size, gravity));
+			drawn += 1;
+
+func _draw() -> void:
+	draw_rect(bounding_rectangle, Color.WHEAT, false, 5);
+	
---
+++ project_1/scripts/water_drop.gd
@@ -0,0 +1,38 @@
+class_name Water_Drop
+extends CharacterBody2D
+
+const my_scene: PackedScene = preload("res://scenes/water_drop.tscn");
+
+@export var collision_damping : float = 1;
+@export var bounds_size : Rect2;
+@export var particle_size : float = 3; 
+@export var gravity: float = 980;
+@export var starting_position: Vector2 = Vector2(0, 0);
+
+static func new_water_drop(startingPosition: Vector2, collisionDamping: float, boundsSize : Rect2, particleSize: float, gravity: float) -> Water_Drop:
+	var new_water_drop: Water_Drop = my_scene.instantiate();
+	new_water_drop.collision_damping = collisionDamping;
+	new_water_drop.bounds_size = boundsSize;
+	new_water_drop.particle_size = particleSize;
+	new_water_drop.starting_position = startingPosition;
+	return new_water_drop;
+	
+func _on_sprite_2d_ready() -> void:
+	scale *= particle_size;
+	position = starting_position;
+
+func resolve_collision() -> void: 
+	var bounds_adjusted = abs(bounds_size.position) - Vector2(1.0, 1.0) * particle_size;
+	
+	if(abs(position.x) > bounds_adjusted.x):
+		position.x = bounds_adjusted.x * sign(position.x);
+		velocity.x *= -1.0 * collision_damping;
+	
+	if(abs(position.y) > bounds_adjusted.y):
+		position.y = bounds_adjusted.y * sign(position.y);
+		velocity.y *= -1.0 * collision_damping;
+
+func _physics_process(delta: float) -> void:
+	velocity += Vector2.DOWN * gravity * delta;
+	position += velocity * delta;
+	resolve_collision();
